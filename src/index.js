45d22acfcdb2ee4185b6b1c781eed9a061c4298a
Tim Empey <timothyempey@gmail.com>
2022-09-07 15:42:10 -0700

    add styling
    Joseph Jackson <josephjacksonw@gmail.com>

--- src/index.js
+++ src/index.js
@@ -22,60 +22,94 @@ function getToken(ids, intervals, num) {
 function printElements(response, intervals, num) {
   response.forEach(function(cryptoOutput) {
     let p = document.createElement("h4");
-    p.innerText = cryptoOutput.currency;
+    p.innerText = "\n" + cryptoOutput.currency;
     if (num === 1) {
       document.getElementById("outputs").appendChild(p);
     } else {
       document.getElementById("outputs2").appendChild(p);
     }
+    let img = document.createElement("img");
+    img.src = cryptoOutput.logo_url;
+    if (num === 1) {
+      document.getElementById("outputs").appendChild(img);
+    } else {
+      document.getElementById("outputs2").appendChild(img);
+    }
+
+    let p4 = document.createElement("p");
+    p4.innerText = "Popularity: " + cryptoOutput.rank;
+    if (num === 1) {
+      document.getElementById("outputs").appendChild(p4);
+    } else {
+      document.getElementById("outputs2").appendChild(p4);
+    }
+
     let p1 = document.createElement("p");
-    p1.innerText = "Current Price: $" + cryptoOutput.price;
+    p1.innerText = "Current Price: $" + Math.round(100 * cryptoOutput.price)/100;
     if (num === 1) {
       document.getElementById("outputs").appendChild(p1);
     } else {
       document.getElementById("outputs2").appendChild(p1);
     }
+
+    let p5 = document.createElement("p");
+    p5.innerText = "Market Cap: $" + cryptoOutput.market_cap;
+    if (num === 1) {
+      document.getElementById("outputs").appendChild(p5);
+    } else {
+      document.getElementById("outputs2").appendChild(p5);
+    }
+
+    let p6 = document.createElement("p");
+    p6.innerText = "All Time High: $" + Math.round(100 * cryptoOutput.high)/100;
+    if (num === 1) {
+      document.getElementById("outputs").appendChild(p6);
+    } else {
+      document.getElementById("outputs2").appendChild(p6);
+    }
+
     let p2 = document.createElement("p");
     if (intervals === "1d") {
-      p2.innerText = "1 Day Change: $" + cryptoOutput["1d"].price_change;
+      p2.innerText = "1 Day Change: $" + (Math.round(100 * cryptoOutput["1d"].price_change)/100);
     } else if (intervals === "7d") {
-      p2.innerText = "1 Week Change: $" + cryptoOutput["7d"].price_change;
+      p2.innerText = "1 Week Change: $" + (Math.round(100 * cryptoOutput["7d"].price_change)/100);
     } else if (intervals === "30d") {
-      p2.innerText = "1 Month Change: $" + cryptoOutput["30d"].price_change;
+      p2.innerText = "1 Month Change: $" + (Math.round(100 * cryptoOutput["30d"].price_change)/100);
     } else if (intervals === "365d") {
-      p2.innerText = "1 Year Change: $" + cryptoOutput["365d"].price_change
+      p2.innerText = "1 Year Change: $" + (Math.round(100 * cryptoOutput["365d"].price_change)/100);
     } else {
-      p2.innerText = "Year To Date: $" + cryptoOutput["ytd"].price_change
+      p2.innerText = "Year To Date: $" + (Math.round(100 * cryptoOutput["ytd"].price_change)/100);
     }
     if (num === 1) {
       document.getElementById("outputs").appendChild(p2);
     } else {
       document.getElementById("outputs2").appendChild(p2);
     }
+    if (p2.innerText.includes("-")){
+      p2.setAttribute("class", "negative");
+    } else {
+      p2.setAttribute("class", "positive");
+    }
   });
 }
 
-
-
-
-
 function handleForm(e) {
   e.preventDefault();
-  let outputs = document.getElementById("outputs")
-  outputs.innerHTML = null
-  let idsArray = []
+  let outputs = document.getElementById("outputs");
+  outputs.innerHTML = null;
+  let idsArray = [];
   let ids = document.querySelectorAll("input[type=checkbox]:checked");
   let intervals = document.getElementById("intervals").value;
   for (let i = 0; i < ids.length; i++) {
-    idsArray.push(ids[i].value)
+    idsArray.push(ids[i].value);
   }
   getToken(idsArray, intervals, 1);
 }
 
 function handleForm2(e) {
   e.preventDefault();
-  let outputs = document.getElementById("outputs2")
-  outputs.innerHTML = null
+  let outputs = document.getElementById("outputs2");
+  outputs.innerHTML = null;
   let intervals = document.getElementById("intervals2").value;
   let altId = document.querySelector("select#alt").value;
   getToken(altId, intervals, 2);
